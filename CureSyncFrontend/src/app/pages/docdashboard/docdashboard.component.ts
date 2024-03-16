091740f12c579e0887bdb0bf2b4a3cb1a7f51a8e
Mushtaq-tech <mohdmush2@gmail.com>
2024-03-17 01:16:34 +0530

    styling plus add prescription

--- CureSyncFrontend/src/app/pages/docdashboard/docdashboard.component.ts
+++ CureSyncFrontend/src/app/pages/docdashboard/docdashboard.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-docdashboard',
   templateUrl: './docdashboard.component.html',
-  styleUrl: './docdashboard.component.css'
+  styleUrls: ['./docdashboard.component.css']
 })
 export class DocdashboardComponent {
+
+  toggleFileInput(doctor: string) {
+    const fileInput = document.getElementById(`${doctor}Input`) as HTMLInputElement;
+    if (fileInput) {
+      fileInput.click(); // Trigger click event on file input
+    }
+  }
+
+  uploadFile(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const files = inputElement.files;
+    if (files && files.length > 0) {
+      const fileToUpload = files[0]; // Get the first file from the FileList
+      // Implement your file upload logic here
+    }
+  }
 
 }
